f8561d32d33a3b8c9380dc9987eb31d660936f0f
wagnerDestro <wagner.leandro28@outlook.com>
2021-02-23 17:26:20 -0300

    Atualização de funcionalidade
    Foi feito o script responsável por permitir que o usuário insira um texto.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
 
@@ -8,7 +8,7 @@ export default function App() {
 
 
   const [estado, setarEstado] = useState('leitura');
-  const [anotacao, setarAnotaco] = useState('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+  const [anotacao, setarAnotacao] = useState('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
 
   if (estado == 'leitura'){
     return (
@@ -26,7 +26,8 @@ export default function App() {
         <StatusBar style="light" />
       <View style={styles.header}><Text style={{textAlign:'center', color:'white', fontSize:18}}>Aplicativo Anotação</Text></View>    
       
-
+      <TextInput style={{height:300, textAlignVertical:'top', padding:20}} onChangeText={(text) => setarAnotacao(text)} multiline={true} numberOfLines={5} value={anotacao}></TextInput>
+      
       <TouchableOpacity onPress={() => setarEstado('leitura')} style={styles.btnSalvar}><Text style={{textAlign:'center', color:'white'}}>Salvar</Text></TouchableOpacity>
       </View>
     );
