059a40a317c34357b81ca627b97e6dd0736be31d
wagnerDestro <wagner.leandro28@outlook.com>
2021-02-24 20:04:08 -0300

    Possibilidade de salvar
    Foi implementada a possibilidade do usuário salvar as anotações mesmo após fechar a aplicação.

--- App.js
+++ App.js
@@ -1,34 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput, AsyncStorage } from 'react-native';
 
 export default function App() {
 
+  useEffect( () =>{
+    //Quando inicalizar o app, queremos que leia a key anotacao.
 
+    (async ()=>{
+      try{
+        const anotacaoLeitura = await AsyncStorage.getItem('anotacao');
+        setarAnotacao(anotacaoLeitura);
+      }
+      catch(error){
+
+      }
+
+    })();
+
+  }, [])
 
 
   const [estado, setarEstado] = useState('leitura');
-  const [anotacao, setarAnotacao] = useState('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+  const [anotacao, setarAnotacao] = useState('');
+
+  setData = async() => {
+    try{
+      await AsyncStorage.setItem('anotacao', anotacao);
+
+
+      alert('Sua anotação foi salva!');
+
+    }catch(error){
+
+    }
+
+
+  }
+
+
+  function atualizarTexto(){
+    setarEstado('leitura');
+    setData();
+  }
 
   if (estado == 'leitura'){
     return (
       <View style={{flex:1}}>
-        <StatusBar style="light" />
+        <StatusBar hidden />
       <View style={styles.header}><Text style={{textAlign:'center', color:'white', fontSize:18}}>Aplicativo Anotação</Text></View>    
-      <View style={{padding:20}}><Text style={styles.anotacao}>{anotacao}</Text></View>
+      
+      
+      {
+        (anotacao != '')?
+        <View style={{padding:20}}><Text style={styles.anotacao}>{anotacao}</Text></View>
+        :
+        <View style={{padding:20}}><Text style={styles.anotacao}>Nenhuma anotação encontrada</Text></View>
+      }
 
-      <TouchableOpacity onPress={() => setarEstado('atualizando')} style={styles.btnAnotacao}><Text style={styles.btnAnotacaoTexto}>+</Text></TouchableOpacity>
+      <TouchableOpacity onPress={() => setarEstado('atualizando')} 
+      style={styles.btnAnotacao}>
+        {
+          (anotacao == "")?
+          <Text style={styles.btnAnotacaoTexto}>+</Text>
+          :
+          <Text style={{fontSize:12, color:'white', textAlign:'center',marginTop:16}} >Editar</Text>
+        }
+      </TouchableOpacity>
       </View>
     );
   }else if (estado == 'atualizando'){
     return (
       <View style={{flex:1}}>
-        <StatusBar style="light" />
+        <StatusBar hidden />
       <View style={styles.header}><Text style={{textAlign:'center', color:'white', fontSize:18}}>Aplicativo Anotação</Text></View>    
       
-      <TextInput style={{height:300, textAlignVertical:'top', padding:20}} onChangeText={(text) => setarAnotacao(text)} multiline={true} numberOfLines={5} value={anotacao}></TextInput>
+      <TextInput autoFocus={true} style={{height:300, textAlignVertical:'top', padding:20}} onChangeText={(text) => setarAnotacao(text)} multiline={true} numberOfLines={5} value={anotacao}></TextInput>
       
-      <TouchableOpacity onPress={() => setarEstado('leitura')} style={styles.btnSalvar}><Text style={{textAlign:'center', color:'white'}}>Salvar</Text></TouchableOpacity>
+      <TouchableOpacity onPress={() => atualizarTexto()} style={styles.btnSalvar}><Text style={{textAlign:'center', color:'white'}}>Salvar</Text></TouchableOpacity>
       </View>
     );
   }
@@ -37,7 +86,7 @@ export default function App() {
 const styles = StyleSheet.create({
   header:{
     width:'100%',
-    paddingTop:30,
+    paddingTop:15,
     backgroundColor: '#069'
   },
 
